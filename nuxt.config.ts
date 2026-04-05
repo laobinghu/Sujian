26ef7f575b641b8d1b012621e092c9a9750027f4
laobinghu <923190468@qq.com>
2026-04-06 01:22:22 +0800

    feat: 在构建配置中添加 sourcemap 选项

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,6 +78,7 @@ export default defineNuxtConfig({
     },
     logLevel: 'error',
     build: {
+      sourcemap: false,
       chunkSizeWarningLimit: 1000,
       rollupOptions: {
         onwarn(warning, warn) {
